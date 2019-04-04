e166b99754118085456e1dc386ca40915de27ab9
Rylee Green <rgreen@aspen-systems.com>
2019-04-04 08:37:31 -0700

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,7 +51,7 @@ namespace GradeBook.UserInterfaces
                      gradeBook = new RankedGradeBook(name);
                     break;
                 default:
-                    Console.WriteLine(type + " is not a supported type of gradebook. Please try again.");
+                    Console.WriteLine(type + " is not a supported type of gradebook, please try again");
                     return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
